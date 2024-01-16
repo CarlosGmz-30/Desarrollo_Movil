35d5a9a2a92a85dd4f20f06fa9a424d2f38f13ad
Carlos gmz <20223tn066@utez.edu.mx>
2024-01-16 08:35:51 -0600

    Js

---
+++ WEB/js/script.js
@@ -0,0 +1,67 @@
+/*
+
+VARIABLES Y CONSTANTES:
+
+const: Constantes
+let: Variables
+var: Variables que no cuenta con scope
+¿Qué es Scope? Es el alcance de un bloque de código {}
+
+*/
+
+/*
+ 
+FUNCIONES:
+
+function Suma(numero1, numero2) {
+    return numero1 + numero2;
+};
+
+const resta = function (numero1, numero2) {
+    return numero1 - numero2;
+};
+ 
+const division = (nummero1, numero 2) => {
+    return numero1/numero2;
+};
+
+const division = (numero1, numero2) => numero1/numero2;
+
+(() => {})();
+
+*/
+
+/*
+
+OBJETOS: ARREGLOS Y LISTAS
+
+ARREGLOS:
+const frutas = ['Platano', 'Sandía', 'Mango', true, 22, 50.5]; 
+Index - 0, 1, 2, 3. 4, 5
+lenght - 6
+
+frutas.forEach((element, index) => {
+    console.log("Fruta: " + index + " " + element);
+    console.log(`Fruta: ${index} ${element}`); 
+});
+
+LISTA:
+const person = {
+    name: '',
+    address: '' {
+        lattirud: '',
+        longitude: '',
+        strre: '',
+        number: 0
+    },
+    lastname: '',
+    birthday: '',
+    socialNetwork: {
+        facebook: '',
+        x: ''
+    }
+}
+
+console.log(person.socialNetwork.facebook)
+
+*/
