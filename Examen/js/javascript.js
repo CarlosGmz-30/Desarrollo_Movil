f49f794cf7c939ffc8a709b9323db529c97183cd
Carlos gmz <20223tn066@utez.edu.mx>
2024-01-25 13:57:27 -0600

    Diseño terminado

---
+++ Examen/js/javascript.js
@@ -0,0 +1,29 @@
+
+/*
+const: Constantes
+let: Variables
+var: Variables que no cuenta con scope
+¿Qué es Scope? Es el alcance de un bloque de código {}
+*/
+
+/*
+ 
+FUNCIONES:
+
+function Suma(numero1, numero2) {
+    return numero1 + numero2;
+};
+
+const resta = function (numero1, numero2) {
+    return numero1 - numero2;
+};
+ 
+const division = (nummero1, numero 2) => {
+    return numero1/numero2;
+};
+
+const division = (numero1, numero2) => numero1/numero2;
+
+(() => {})();
+
+*/
