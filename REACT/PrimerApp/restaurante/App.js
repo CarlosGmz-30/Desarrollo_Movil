c1e60be6e37a84e44e03c73a5f551db6efe2e1da
Carlos-gmz <20223tn066@utez.edu.mx>
2024-01-30 08:51:17 -0600

    React

---
+++ REACT/PrimerApp/restaurante/App.js
@@ -0,0 +1,22 @@
+import {} from 'react'
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Carlos Martínez Gómez!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center', // Horizontal
+    justifyContent: 'center', // Vertical
+  },
+});
